1f587fe54e781de5b777781bc8444edca476a221
hyebin <xxico89@gmail.com>
2024-12-03 20:30:31 +0900

    Feat: 붕어빵 캘린더 상세 조회하기 API 구현 - userUUID 쿼리 스트링 대신 @PathVariable 방식으로 변경

--- src/main/java/fish/common/fishBun/controller/FishBunController.java
+++ src/main/java/fish/common/fishBun/controller/FishBunController.java
@@ -1,11 +1,12 @@
 package fish.common.fishBun.controller;
 
+import fish.common.fishBun.dto.response.CalendarDetailResDTO;
 import fish.common.fishBun.dto.response.CalendarResDTO;
 import fish.common.fishBun.service.FishBunService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,10 +16,15 @@ import java.util.List;
 public class FishBunController {
     private final FishBunService fishBunService;
 
-    @GetMapping(value = "/fish-bun/calendar")
-    public ResponseEntity<List<CalendarResDTO>> getCalendarList(@RequestParam String userUUID) {
+    @GetMapping(value = "/fish-bun/calendar/{userUUID}")
+    public ResponseEntity<List<CalendarResDTO>> getCalendarList(@PathVariable("userUUID") String userUUID) {
         List<CalendarResDTO> data = fishBunService.findAllCalendarDate(userUUID);
 
         return ResponseEntity.ok(data);
+    }
+
+    @GetMapping(value = "/fish-bun/calendar/detail/{calendarId}")
+    public ResponseEntity<CalendarDetailResDTO> getCalendarDetail(@PathVariable("calendarId") Long calendarId) {
+        return ResponseEntity.ok(fishBunService.findCalendarDetail(calendarId));
     }
 }
