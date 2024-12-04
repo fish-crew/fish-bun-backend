4f2816157a8bcb72dddb6505d17725b20ed78608
hyebin <xxico89@gmail.com>
2024-12-04 15:56:26 +0900

    Chore: 공통 요청 url 부분 분리

--- src/main/java/fish/common/fishBun/controller/FishBunController.java
+++ src/main/java/fish/common/fishBun/controller/FishBunController.java
@@ -5,6 +5,7 @@ import fish.common.fishBun.service.FishBunService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +13,11 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/fish-bun")
 public class FishBunController {
     private final FishBunService fishBunService;
 
-    @GetMapping(value = "/fish-bun/calendar")
+    @GetMapping(value = "/calendar")
     public ResponseEntity<List<CalendarResDTO>> getCalendarList(@RequestParam String userUUID) {
         List<CalendarResDTO> data = fishBunService.findAllCalendarDate(userUUID);
 
