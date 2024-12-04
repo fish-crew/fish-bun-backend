9a71e426bcdba5d208bfda160871dca27414bddc
hyebin <xxico89@gmail.com>
2024-12-05 01:43:27 +0900

    Chore: Repository name 변경

--- src/main/java/fish/common/fishBun/repository/FishBunBookRepository.java
+++
@@ -1,9 +0,0 @@
-package fish.common.fishBun.repository;
-
-import fish.common.fishBun.entity.FishBunFlavor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FishBunBookRepository extends JpaRepository<FishBunFlavor, Long> {
-}
