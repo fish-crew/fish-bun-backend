e50cce4f2bbe7ab063f0d967fa88e22c0b703e44
seungwonni <eutj47@gmail.com>
2024-12-14 19:03:47 +0900

    Fix: 붕어빵 로그인 수정사항
    - 로그인 Flow 변경
      - 필터 기능 추가하여 인증 받은 Token만 처리하도록 변경
      - 로그인 성공 시에 Http Cookie를 통하여 token을 전달하도록 기능 추가
      - AuthenticationPrincipal에 User를 inject 받도록 수정

--- src/main/java/fish/core/config/LoginAuthHandler.java
+++ src/main/java/fish/core/config/LoginAuthHandler.java
@@ -2,38 +2,79 @@ package fish.core.config;
 
 import fish.common.history.user.entity.UserHistory;
 import fish.common.history.user.serivce.UserHistoryService;
+import fish.common.user.entity.User;
+import fish.common.user.service.UserService;
 import fish.core.util.IpAddressUtil;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class LoginAuthHandler implements AuthenticationSuccessHandler, AuthenticationFailureHandler {
+public class LoginAuthHandler extends SimpleUrlAuthenticationSuccessHandler
+        implements AuthenticationSuccessHandler, AuthenticationFailureHandler {
 
     private final UserHistoryService userHistoryService;
+    private final UserService userService;
+    private final OAuth2AuthorizedClientService authorizedClientService;
+    @Value("${front-url}")
+    private String frontUrl;
+    private final static String MAIN_URL = "/main";
+    private final static String NICKNAME_URL = "/nicknamePage";
+    private final static Integer LOGIN_SESSION = 6 * 60 * 60;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException {
-        String ip = IpAddressUtil.getClientIp(request);
+        long providerId = Long.parseLong(authentication.getName());
         //로그인 이력 히스토리 쌓음
-        userHistoryService.saveHistory(new UserHistory(Long.valueOf(authentication.getName()), ip));
+        userHistoryService.saveHistory(
+                new UserHistory(providerId , IpAddressUtil.getClientIp(request)
+                )
+        );
+        //유저 값 가져와서
+        User user = userService.getUserById(providerId);
+        response.addCookie(setCookie(authentication));
+        //유저가 닉네임이 등록되어있을 시에 Main으로 리턴, 존재하지 않을 시 Nickname 등록 URL로 리턴
+        String path = user.getNickname() != null ? MAIN_URL : NICKNAME_URL;
+        String redirectUrl = frontUrl + path;
+        getRedirectStrategy().sendRedirect(request, response, redirectUrl);
+    }
+
+    private Cookie setCookie(Authentication authentication) {
         // 리다이렉트 처리 (예: 홈 페이지로 이동)
-        response.sendRedirect("/login/success");
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        // 클라이언트 정보 가져오기
+        String clientRegistrationId = oauthToken.getAuthorizedClientRegistrationId();
+        // OAuth2AuthorizedClient 가져오기
+        OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient(
+                clientRegistrationId, oauthToken.getName());
+        // Access Token을 HttpOnly Cookie로 저장
+        Cookie cookie = new Cookie("accessToken", authorizedClient.getAccessToken().getTokenValue());
+        cookie.setHttpOnly(true); // JavaScript에서 접근 불가능
+        cookie.setSecure(true); // HTTPS 환경에서만 전송 (필수)
+        cookie.setPath("/");
+        cookie.setMaxAge(LOGIN_SESSION); // 3시간 유효
+        return cookie;
     }
 
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception)
             throws IOException {
-
-        response.sendRedirect("/login");
+        response.sendRedirect("/loginPage");
     }
 }
--- src/main/java/fish/core/config/SecurityConfig.java
+++ src/main/java/fish/core/config/SecurityConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @RequiredArgsConstructor
@@ -15,10 +16,16 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final OAuthUserService oAuthUserService;
+    private final OAuth2TokenFilter oAuth2TokenFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, LoginAuthHandler authHandler) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
+        http.csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringRequestMatchers(
+                        "/fish-bun/**" // CSRF 비활성화 경로
+                )
+        );
 
         http.authorizeHttpRequests(authorize -> {
             authorize
@@ -29,9 +36,10 @@ public class SecurityConfig {
                                     , "/images/**"
                                     , "/js/**"
                             ).permitAll() // 인증 없이 접근 가능
-                    .requestMatchers("/fish-bun/**").authenticated()
+                   .requestMatchers("/fish-bun/**").authenticated()
             ;
         });
+
         http.oauth2Login(form -> {
             form
                     .userInfoEndpoint(userInfoEndpointConfig -> {
@@ -40,6 +48,11 @@ public class SecurityConfig {
                     .successHandler(authHandler)
                     .failureHandler(authHandler);
         });
+        http.requestCache();
+        http.addFilterBefore(
+                oAuth2TokenFilter, OAuth2AuthorizationRequestRedirectFilter.class
+        );
+
         return http.build();
     }
 }
