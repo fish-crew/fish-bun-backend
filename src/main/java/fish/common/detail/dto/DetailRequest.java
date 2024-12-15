4ef96f5aaec93dcb6d61bf902d878389cf3b1609
seungwonni <eutj47@gmail.com>
2024-12-15 19:52:01 +0900

    Fixed: detailFlavor 값 수정

--- src/main/java/fish/common/detail/dto/DetailRequest.java
+++ src/main/java/fish/common/detail/dto/DetailRequest.java
@@ -4,16 +4,17 @@ import fish.common.detail.entity.DetailEntity;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
 
 @Data
 @RequiredArgsConstructor
 public class DetailRequest {
-    private String flavors;
+    private List<DetailFlavor> flavors;
     private MultipartFile picture;
 
     public DetailEntity toEntity(DetailRequest request, Long userId) {
         return DetailEntity.builder()
-                .flavors(request.flavors)
+                .flavors(request.flavors.toString())
                 .userId(userId)
                 .build();
     }
