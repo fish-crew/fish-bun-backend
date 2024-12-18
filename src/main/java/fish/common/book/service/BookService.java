6bfa5662521387b821387bdf67b8ee7f09928ea1
hyebin <xxico89@gmail.com>
2024-12-18 16:44:16 +0900

    Feat: 사용자 붕어빵 도감 데이터 조회 응답값 추가
    - iconCode, seq

--- src/main/java/fish/common/book/service/BookService.java
+++ src/main/java/fish/common/book/service/BookService.java
@@ -3,20 +3,30 @@ package fish.common.book.service;
 import fish.common.book.entity.UserBook;
 import fish.common.book.repository.UserBookRepository;
 import fish.common.book.response.UserBookResponse;
+import fish.common.detail.response.DetailResponse;
+import fish.common.flavor.entity.FishBunFlavor;
+import fish.common.flavor.repository.FlavorRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class BookService {
     private final UserBookRepository userBookRepository;
+    private final FlavorRepository flavorRepository;
 
     public List<UserBookResponse> findAllUserFishBunBook(Long userId) {
         return userBookRepository.findAllByUserId(userId).stream()
-                .map(UserBookResponse::toResponseDTO)
-                .toList();
+                .map((userBook) -> {
+                    Long flavorId = userBook.getCompletedFlavorId();
+                    FishBunFlavor flavor = flavorRepository.findById(flavorId).orElseThrow(() -> new EntityNotFoundException("FishBun Flavor Entity not found with ID: " + flavorId));
+                    return UserBookResponse.toResponseDTO(userBook, flavor);
+                })
+                .collect(Collectors.toList());
     }
 
     public void saveUserCompletedFlavor(List<Long> flavorIdList, Long userId) {
