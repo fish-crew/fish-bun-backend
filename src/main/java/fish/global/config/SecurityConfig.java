f8f5fd91414ec928469d3c762877a4af0e4a73a2
seungwonni <eutj47@gmail.com>
2025-02-02 23:48:48 +0900

    프론트 작업 시에 cors 에러 방지를 위해 추가

--- src/main/java/fish/global/config/SecurityConfig.java
+++ src/main/java/fish/global/config/SecurityConfig.java
@@ -25,7 +25,10 @@ public class SecurityConfig {
         http.cors(cors -> cors
                 .configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(List.of("https://bunglog.me"));
+                    configuration.setAllowedOrigins(List.of(
+                            "https://bunglog.me"
+                            , "http://localhost:3000"
+                    ));
                     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     configuration.setAllowedHeaders(List.of("*"));
                     configuration.setAllowCredentials(true);
