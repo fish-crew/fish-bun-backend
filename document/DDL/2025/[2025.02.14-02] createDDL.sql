de125c1d3385a121da014d697e0fa990caee33af
hyebin <xxico89@gmail.com>
2025-02-15 02:12:14 +0900

    Document: 커뮤니티용 테이블 create DDL 추가

---
+++ document/DDL/2025/[2025.02.14-02] createDDL.sql
@@ -0,0 +1,38 @@
+CREATE TABLE `POST` (
+	`id` bigint NOT NULL AUTO_INCREMENT,
+  	`title` varchar(50) NOT NULL,
+ 	`content` varchar(500) DEFAULT NULL,
+ 	`firstOption` varchar(30) DEFAULT NULL,
+ 	`secondOption` varchar(30) DEFAULT NULL,
+ 	`firstOptionCount` bigint DEFAULT 0,
+    `secondOptionCount` bigint DEFAULT 0,
+	`regDate` datetime(6) DEFAULT NULL,
+  	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+;
+
+
+CREATE TABLE `COMMENT` (
+	`id` bigint NOT NULL AUTO_INCREMENT,
+ 	`content` varchar(500) DEFAULT NULL,
+ 	`likeCount` bigint DEFAULT 0,
+ 	`postId` bigint NOT NULL,
+    `userId` bigint NOT NULL,
+    `userNickName` varchar(15) DEFAULT NULL,
+	`regDate` datetime(6) DEFAULT NULL,
+  	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+;
+
+
+
+CREATE TABLE `VOTE` (
+	`id` bigint NOT NULL AUTO_INCREMENT,
+  	`title` varchar(50) NOT NULL,
+  	`postId` bigint NOT NULL,
+    `userId` bigint NOT NULL,
+    `voteOption` varchar(30) DEFAULT NULL,
+	`regDate` datetime(6) DEFAULT NULL,
+  	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+;
