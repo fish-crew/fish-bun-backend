566457905ba36a7ff1705f445533b3cc88cf6113
hyebin <xxico89@gmail.com>
2025-02-16 16:35:04 +0900

    Document: Add createDDL.sql

--- document/DDL/2025/[2025.02.14-02] createDDL.sql
+++ document/DDL/2025/[2025.02.14-02] createDDL.sql
@@ -1,12 +1,11 @@
 CREATE TABLE `POST` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
   	`title` varchar(50) NOT NULL,
- 	`content` varchar(500) DEFAULT NULL,
+ 	`contents` varchar(500) DEFAULT NULL,
  	`firstOption` varchar(30) DEFAULT NULL,
  	`secondOption` varchar(30) DEFAULT NULL,
- 	`firstOptionCount` bigint DEFAULT 0,
-    `secondOptionCount` bigint DEFAULT 0,
 	`regDate` datetime(6) DEFAULT NULL,
+	`fileIdList` varchar(50) DEFAULT NULL,
   	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 ;
@@ -14,7 +13,7 @@ CREATE TABLE `POST` (
 
 CREATE TABLE `COMMENT` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
- 	`content` varchar(500) DEFAULT NULL,
+ 	`contents` varchar(500) DEFAULT NULL,
  	`likeCount` bigint DEFAULT 0,
  	`postId` bigint NOT NULL,
     `userId` bigint NOT NULL,
