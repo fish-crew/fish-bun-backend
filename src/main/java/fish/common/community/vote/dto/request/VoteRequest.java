0e185448f9402af3e69c523ae978a8f4a760d440
hyebin <xxico89@gmail.com>
2025-03-02 21:08:47 +0900

    Feat: 실시간 투표 현황 조회 API

---
+++ src/main/java/fish/common/community/vote/dto/request/VoteRequest.java
@@ -0,0 +1,8 @@
+package fish.common.community.vote.dto.request;
+
+import lombok.Data;
+
+@Data
+public class VoteRequest {
+    private String voteOption;
+}
---
+++ src/main/java/fish/common/community/vote/service/VoteService.java
@@ -0,0 +1,33 @@
+package fish.common.community.vote.service;
+
+import fish.common.community.vote.dto.request.VoteRequest;
+import fish.common.community.vote.entity.VoteEntity;
+import fish.common.community.vote.repository.VoteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class VoteService {
+    private final VoteRepository voteRepository;
+
+    @Transactional
+    public List<Map<String, Object>> saveVote(Long postId, Long userId, VoteRequest request) {
+        Optional<VoteEntity> vote = voteRepository.findByPostIdAndUserId(postId, userId);
+        // 중복 투표의 경우 기존 투표 취소
+        if (vote.isPresent()) {
+            voteRepository.delete(vote.get());
+        }
+        else {
+            voteRepository.save(VoteEntity.toEntity(postId, userId, request));
+        }
+
+        // 현재 집계 수 소켓 메세지로 보내주기
+        return voteRepository.countVotesByPostId(postId);
+    }
+}
